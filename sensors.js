241be0a2d892f44ff76c8ae880b15fa64287f247
Matthew <matthewmoorhead@yahoo.com>
2016-09-12 15:05:22 -0400

    Add files via upload

--- sensors.js
+++ sensors.js
@@ -2,19 +2,19 @@ var handleOrientationEvent = function(e){
     var alpha = e.alpha,
         beta = e.beta
         gamma = e.gamma;
-    document.getElementById("gx").innerHTML = 'X: ' + alpha;
-    document.getElementById("gy").innerHTML = 'Y: ' + beta;
-    document.getElementById("gz").innerHTML = 'Z: ' + gamma;
+    document.getElementById("gx").innerHTML = '<b>X:</b> ' + alpha;
+    document.getElementById("gy").innerHTML = '<b>Y:</b> ' + beta;
+    document.getElementById("gz").innerHTML = '<b>Z:</b> ' + gamma;
 };
 
 var handleDeviceMotionEvent = function(e){
-    var acc = e.acceleration,
+    var acc = e.accelerationIncludingGravity,
         x = acc.x,
         y = acc.y,
         z = acc.z;
-    document.getElementById("ax").innerHTML = 'X: ' + x;
-    document.getElementById("ay").innerHTML = 'Y: ' + y;
-    document.getElementById("az").innerHTML = 'Z: ' + z;
+    document.getElementById("ax").innerHTML = '<b>X:</b> ' + x;
+    document.getElementById("ay").innerHTML = '<b>Y:</b> ' + y;
+    document.getElementById("az").innerHTML = '<b>Z:</b> ' + z;
 }
 window.addEventListener('deviceorientation', handleOrientationEvent, false);
 window.addEventListener('devicemotion', handleDeviceMotionEvent, false);
